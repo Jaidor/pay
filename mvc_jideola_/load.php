d39df615e71923e1065b680d2987bb195bb9d336
Jaidor <sundayolajide46@gmail.com>
2021-10-01 14:03:11 -0700

    Updates

--- mvc_jideola_/load.php
+++ mvc_jideola_/load.php
@@ -11,8 +11,8 @@ header('Content-Type: application/json');
 
 header("Access-Control-Allow-Origin: *");
 // header("Access-Control-Allow-Credentials", 'true');
-// header("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE, OPTIONS');
-// header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE, OPTIONS');
+header("Access-Control-Allow-Headers: *");
 
 $header =  apache_request_headers();
 $xAuthToken =  (isset($header['X-Authorization']))? $header['X-Authorization'] : '';  /* Some servers uses X-Authorization  */
