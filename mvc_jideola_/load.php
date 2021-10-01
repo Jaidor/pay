321df3ce8bf20cc62a02dd17b1d9331b6832d2b1
Jaidor <sundayolajide46@gmail.com>
2021-10-01 13:44:37 -0700

    Updates

--- mvc_jideola_/load.php
+++ mvc_jideola_/load.php
@@ -10,7 +10,7 @@ header('Content-Type: application/json');
 // }
 
 // header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Credentials", 'true');
+header("Access-Control-Allow-Credentials", 'true');
 header("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE, OPTIONS');
 header("Access-Control-Allow-Headers: *");
 
