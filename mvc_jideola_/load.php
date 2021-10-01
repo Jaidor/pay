0a429933896274d1f01dca9cf02206b4cb49d77f
Jaidor <sundayolajide46@gmail.com>
2021-10-01 11:52:48 -0700

    Updates

--- mvc_jideola_/load.php
+++ mvc_jideola_/load.php
@@ -16,7 +16,7 @@ $software->setToken($finalToken); /* Set session  */
 $request_link = $_SERVER['REQUEST_URI'];
 $request_link = explode('/',$request_link);
 
-print_r($_SERVER);
+print_r($request_link);
 // unset($request_link[0]);
 
 // $method = $software->antiHacking($request_link[2]);
