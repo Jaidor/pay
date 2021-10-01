09535cac3400e480d3672aa6386bef9e04b92ccd
Jaidor <sundayolajide46@gmail.com>
2021-10-01 11:26:26 -0700

    Updates

--- inc_jideola_/index.php
+++ inc_jideola_/index.php
@@ -2,31 +2,30 @@
 ob_start();
 session_start();
 
-echo "I got here...";
+/* 
+* Display errors 
+*/
 
-// /* 
-// * Display errors 
-// */
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
 
-// ini_set('display_errors', 1); 
-// ini_set('display_startup_errors', 1); 
-// error_reporting(E_ALL);
+/* 
+* Include files 
+*/
+if(file_exists(__DIR__.'/db.php')) {
+	$params = include(__DIR__.'/db.php');
+}
 
-// /* 
-// * Include files 
-// */
-// if(file_exists(__DIR__.'/db.php')) {
-// 	$params = include(__DIR__.'/db.php');
-// }
+foreach (glob(__DIR__."/*.php") as $filename) { 
+	$info = pathinfo($filename); 
+	if ($info["filename"] != "db" && $info["filename"] != "rsa") {
+		if($filename!=__FILE__) include $filename;
+        echo "$filename filesize = " . filesize($filename) . "\n";
+    }
+}
 
-// foreach (glob(__DIR__."/*.php") as $filename) { 
-// 	$info = pathinfo($filename); 
-// 	if ($info["filename"] != "db" && $info["filename"] != "rsa") {
-// 		if($filename!=__FILE__) include $filename;
-//         echo "$filename filesize = " . filesize($filename) . "\n";
-//     }
-// }
-
-// $software = new softwareFunctions($params);
+$software = new softwareFunctions($params);
 // include_once MVC."load.php";
+echo "I got here safely...";
 ?>
