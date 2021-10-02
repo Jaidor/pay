3e6bbcd233aad62ab0123c4cf9a5ee9a76fa8354
Jaidor <sundayolajide46@gmail.com>
2021-10-01 18:46:01 -0700

    Updates

--- mvc_jideola_/api/1.0/src/Controllers/CreatepayController.php
+++ mvc_jideola_/api/1.0/src/Controllers/CreatepayController.php
@@ -9,13 +9,15 @@ class CreatepayController {
     public function payment(){
         global $software;
 
+        print_r($this->request);
+
         /* Validate */
-        $checkArray = ['username', 'firstname', 'surname', 'lastname', 'email', 'phone','address','password','confirmPassword','terms'];
-        foreach ($checkArray as $check){
-            if(isset($this->request[$check])) {
-                if (empty($this->request[$check])) $software->feedback(false, 'REG_000', ['Oops! '.$check.' was not provided.']);
-            }else $software->feedback(false, 'REG_001', ['Oops! '.$check.' was not set.']);
-        }
+        // $checkArray = ['username', 'firstname', 'surname', 'lastname', 'email', 'phone','address','password','confirmPassword','terms'];
+        // foreach ($checkArray as $check){
+        //     if(isset($this->request[$check])) {
+        //         if (empty($this->request[$check])) $software->feedback(false, 'REG_000', ['Oops! '.$check.' was not provided.']);
+        //     }else $software->feedback(false, 'REG_001', ['Oops! '.$check.' was not set.']);
+        // }
     
     }
 }
