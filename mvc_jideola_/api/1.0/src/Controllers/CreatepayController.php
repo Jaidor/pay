b9d09477fda250fe54b0a4564d38538752c6a262
Jaidor <sundayolajide46@gmail.com>
2021-10-02 13:36:01 -0700

    Updates

--- mvc_jideola_/api/1.0/src/Controllers/CreatepayController.php
+++ mvc_jideola_/api/1.0/src/Controllers/CreatepayController.php
@@ -30,6 +30,41 @@ class CreatepayController {
             }else $software->feedback(false, 'PAY_001', ['Oops! '.$check.' was not set.']);
         }
 
+       $quantity = $this->request['payQuan'];
+       $normal_price = $this->request['payNor'];
+       $total = ($normal_price * $quantity);
+
+
+         // connect to mongodb
+        // $m = new MongoClient();
+        // echo "Connection to database successfully";
+            
+        // // select a database
+        // $db = $m->mydb;
+        // echo "Database mydb selected";
+        // $collection = $db->mycol;
+        // echo "Collection selected succsessfully";
+            
+        // $payData = array( 
+        //     "item" => $this->request['payItem'], 
+        //     "category" => $this->request['payCat'], 
+        //     "normalPrice" => $this->request['payNor'],
+        //     "availablePrice" => $this->request['payAvail'],
+        //     "unitPrice" => $this->request['payUnit'],
+        //     "fullname" => $this->request['payFullname'],
+        //     "quantity" => $this->request['payQuan'],
+        //     "paymentType" => $this->request['payChecks'],
+        //     "total" => $total,
+        //     "digit" => $this->request['payDigit']
+        // );
+            
+        // $collection->insert($payData);
+
+        // $client = new MongoDB\Client(
+        //     'mongodb+srv://<username>:<password>@<cluster-address>/test?retryWrites=true&w=majority'
+        // );
+        // $db = $client->test;
+
         $software->feedback(true, 'OK', ['Payment was successful'], []);
     
     }
