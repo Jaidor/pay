916b111d7e4656df88e797d8559c7e8ccba1f98f
Jaidor <sundayolajide46@gmail.com>
2021-10-01 17:56:29 -0700

    Updates

--- mvc_jideola_/api/1.0/src/Controllers/CreatepayController.php
+++ mvc_jideola_/api/1.0/src/Controllers/CreatepayController.php
@@ -10,6 +10,15 @@ class CreatepayController {
         global $software;
 
         echo "I got into create payment safely....";
+        print_r($this->request);
+
+        /* Validate */
+        // $checkArray = ['username', 'firstname', 'surname', 'lastname', 'email', 'phone','address','password','confirmPassword','terms'];
+        // foreach ($checkArray as $check){
+        //     if(isset($this->request[$check])) {
+        //         if (empty($this->request[$check])) $software->feedback(false, 'REG_000', ['Oops! '.$check.' was not provided.']);
+        //     }else $software->feedback(false, 'REG_001', ['Oops! '.$check.' was not set.']);
+        // }
     
     }
 }
