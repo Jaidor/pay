160c4bdb82f93f38a0933c6b46c370df900b197e
Jaidor <sundayolajide46@gmail.com>
2021-10-01 19:08:27 -0700

    Updates

--- mvc_jideola_/api/1.0/src/Controllers/CreatepayController.php
+++ mvc_jideola_/api/1.0/src/Controllers/CreatepayController.php
@@ -10,14 +10,28 @@ class CreatepayController {
         global $software;
 
         print_r($this->request);
+        die();
+
+        // Array
+        // (
+        //     [payQuan] => 4
+        //     [payFullname] => Olaleye Olajide
+        //     [payChecks] => token
+        //     [payDigit] => 234647
+        //     [payItem] => Flipflops
+        //     [payCat] => Florexworld
+        //     [payNor] => 50000
+        //     [payAvail] => 123
+        //     [payUnit] => 20000
+        // )
 
         /* Validate */
-        // $checkArray = ['username', 'firstname', 'surname', 'lastname', 'email', 'phone','address','password','confirmPassword','terms'];
-        // foreach ($checkArray as $check){
-        //     if(isset($this->request[$check])) {
-        //         if (empty($this->request[$check])) $software->feedback(false, 'REG_000', ['Oops! '.$check.' was not provided.']);
-        //     }else $software->feedback(false, 'REG_001', ['Oops! '.$check.' was not set.']);
-        // }
+        $checkArray = ['payQuan', 'payFullname', 'payChecks', 'payDigit', 'payItem', 'payCat','payNor','payAvail','payUnit'];
+        foreach ($checkArray as $check){
+            if(isset($this->request[$check])) {
+                if (empty($this->request[$check])) $software->feedback(false, 'REG_000', ['Oops! '.$check.' was not provided.']);
+            }else $software->feedback(false, 'REG_001', ['Oops! '.$check.' was not set.']);
+        }
     
     }
 }
